2ecdd1edc3c5ba4cae9ff3e94211f308eab67076
Matt Seaton <matt.r.seaton@gmail.com>
2016-12-02 14:06:10 -0700

    added express route

--- server/run.js
+++ server/run.js
@@ -2,13 +2,14 @@ const express = require ("express");
 const app = express();
 const foods = require ("./data.json");
 const bodyParser = require('body-parser');
+const cors = require("cors");
 app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(3000, function() {
   console.log("listening on port 3000");
-})
+});
 
-function speak()  {
-  console.log(foods);
-}
-speak();
+app.get('/foods', function(req, res, next) {
+  res.json(foods);
+});
